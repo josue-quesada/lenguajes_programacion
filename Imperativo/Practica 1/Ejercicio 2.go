2c660b85a092419930fdf936756a108c231eb6e1
Josué Quesada <josuequesadaxd@gmail.com>
2023-03-06 20:02:43 -0600

    Update Ejercicio 2.go

--- Imperativo/Practica 1/Ejercicio 2.go
+++ Imperativo/Practica 1/Ejercicio 2.go
@@ -4,16 +4,24 @@ import "fmt"
 
 func printFigure(n int) {
 	if n > 0 && n%2 != 0 {
-		var i, j int
-		for i = 1; i <= n; i += 2 {
+		var i, j, k int
+		for i = 1; i <= n-n/2; i++ {
 			for j = 1; j <= n-i; j++ {
-				fmt.Print("")
+				print(" ")
 			}
-			for j = 1; j <= i; j++ {
+			for k = 1; k <= i*2-1; k++ {
 				fmt.Print("*")
 			}
 			fmt.Println()
-
+		}
+		for i = n / 2; i > 0; i-- {
+			for j = 1; j <= n-i; j++ {
+				print(" ")
+			}
+			for k = 1; k <= i*2-1; k++ {
+				fmt.Print("*")
+			}
+			fmt.Println()
 		}
 
 	} else {
